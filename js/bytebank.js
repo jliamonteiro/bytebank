6fd8f35c9ffbd99be25a62b152ac24cd6c58d92e
jliamonteiro <jlmonteiro2355@gmail.com>
2025-03-16 19:12:09 -0300

    iniciando funcionalidades com javascript

--- js/bytebank.js
+++ js/bytebank.js
@@ -3,11 +3,44 @@ let saldo = 3000;
 
 const  elementoSaldo = document.querySelector('.saldo-valor .valor');
 elementoSaldo.textContent= saldo;
-const elementoFormulario = document.querySelector('.block-novatransacao form');
-elementoFormulario.addEventListener("submit", function(){
+const elementoFormulario = document.querySelector('.block-nova-transacao form');
+elementoFormulario.addEventListener("submit", function(event){
     event.preventDefault();
     if(!elementoFormulario.checkVisibility()){
         alert("Preencha todos os campos");
         return;
     }
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if( tipoTransacao == "Depósito"){
+        saldo += parseFloat(valor);
+        // caso o valor tivesse sido mantido sem esse parse float na frente, o javascript iria 
+        // concatenar o saldo e o valor ao invés de fzr adição, pois a váriavel esta no tipo string, 
+        // e o js caso tenha um sinal de aição, vai priorizar a concatenação ao ivés do cálculo
+    }else if(tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    }else{
+        alert("Tipo de transação inválido");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo;
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao)
+    elementoFormulario.reset();
+
+
 })
